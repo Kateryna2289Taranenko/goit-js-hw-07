b414d11955d8617d9d0f504bff9efc527bea7284
Kateryna2289Taranenko <katuha2289@ukr.net>
2023-03-05 10:24:26 +0200

    add 1 and 2 tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+console.log(galleryEl);
+function createGalleryItems(elements) {
+  return elements
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+        <a class="gallery__link" 
+        href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+const cardsMarkup = createGalleryItems(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", cardsMarkup);
+galleryEl.addEventListener("click", onGalleryElClick);
+let instance = null;
+function onGalleryElClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  const bigImageModal = evt.target.dataset.source;
+  instance = basicLightbox.create(
+    `<img src="${bigImageModal}" width="800" height="600">`
+  );
+  instance.show(() => {
+    window.addEventListener("keydown", onEscPress);
+  });
+}
+
+function onEscPress(evt) {
+  if (evt.code === "Escape") {
+    instance.close(() => {
+      window.removeEventListener("keydown", onEscPress);
+    });
+  }
+}
